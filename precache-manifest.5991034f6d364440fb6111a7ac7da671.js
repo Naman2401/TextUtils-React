7282bf4e7d4091d3eb19340c5e34600b742f1a8b
Naman Agarwal <agarwalnaman2401@gmail.com>
2023-03-14 19:47:40 +0530

    Updates

---
+++ precache-manifest.5991034f6d364440fb6111a7ac7da671.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a6ad4ebf1826d7a0878",
+    "url": "/TextUtils-React/static/css/main.a17e5443.chunk.css"
+  },
+  {
+    "revision": "0a6ad4ebf1826d7a0878",
+    "url": "/TextUtils-React/static/js/main.0a6ad4eb.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/TextUtils-React/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "7032a6c4eed086cace3c",
+    "url": "/TextUtils-React/static/js/2.7032a6c4.chunk.js"
+  },
+  {
+    "revision": "2591e466b5f270c0c477",
+    "url": "/TextUtils-React/static/js/runtime~main.2591e466.js"
+  },
+  {
+    "revision": "ce752f91c93734aa33e89c1aad2b6ef8",
+    "url": "/TextUtils-React/index.html"
+  }
+];
